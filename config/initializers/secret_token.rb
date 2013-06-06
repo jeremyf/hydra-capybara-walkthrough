3d2c35063d3e23a81c9e2fa83f433cd31c9ed86e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-06-06 11:39:32 -0400

    Initial project creation
    Commands:
    $ rails new hydra-capybara-walkthrough
    $ cd hydra-capybara-walkthrough
    $ git init
    $ git add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraCapybaraWalkthrough::Application.config.secret_token = 'c1b7fc0629af05835f4a3f45c46a757e956f13f5d2bf50615538e708b25f36eefa72bb4c4385cafd578063c99bcbda337c752b24730d2c3caf90f25687fac283'
