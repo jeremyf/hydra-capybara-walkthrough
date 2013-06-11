f7d44ae1bbe2fbb62e72b7f9831cd64ad8ad5fe7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-06-11 13:53:06 -0400

    Adding spec and comments for testing catalog

---
+++ spec/features/catalog_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'Catalog' do
+  it {
+    # Go to /catalog
+    # Click "Login" link
+    # Click "Sign up" link
+    # Fill in "Sign up" form
+    # Click "Sign up" button
+    # It should redirect me to /
+  }
+end
