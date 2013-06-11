1c3b445c3f77e8a87b3675d2539ee0770853f017
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-06-11 14:36:27 -0400

    Writing executable tests for capybara tests

--- spec/features/catalog_spec.rb
+++ spec/features/catalog_spec.rb
@@ -1,12 +1,27 @@
 require 'spec_helper'
 
 describe 'Catalog' do
+  let(:email) { 'hello@world.com' }
+  let(:password) { 'my$3cur3p@$$word' }
   it {
-    # Go to /catalog
-    # Click "Login" link
-    # Click "Sign up" link
-    # Fill in "Sign up" form
-    # Click "Sign up" button
-    # It should redirect me to /
+    visit("/catalog")
+    click_link('Login')
+    click_link('Sign up')
+    within("#new_user") do
+      fill_in("Email", with: email)
+
+      # Capybara was complaining when I had the following:
+      #   fill_in('Password', with: password)
+      # Capybara claimed there were two elements; It was likely counting
+      # the "Password confirmation" as well.
+      fill_in("user[password]", with: password)
+      fill_in("user[password_confirmation]", with: password)
+      click_button("Sign up")
+    end
+    page.should have_content("Welcome aboard")
+    visit("/catalog")
+    within('.util-links-login') do
+      page.should have_content(email)
+    end
   }
 end
