88a5efaf13ae6ca2e6baf1a24d0a15231145de92
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-06-28 11:04:21 -0400

    Extracting #verify_persistence to custom matcher
    While techinically not a custom matcher, this extraction demonstrates
    some of Rspec's configuration options; Namely that we can mixin modules
    at "type of spec" level.

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -30,36 +30,4 @@ describe Thing do
     )
   end
 
-
-  protected
-  def verify_persistence(options = {})
-    path = options.fetch(:fedora_path)
-    template_name = options.fetch(:expected_template)
-    attributes = options.fetch(:attributes)
-
-    actual = response_body_for(path)
-    expected = rendered_template_for(template_name, attributes)
-    expect(actual).to eq(expected)
-  end
-
-  def rendered_template_for(template_name, attributes)
-    template = File.read(Rails.root.join('spec/fixtures/expected_fedora_templates', template_name))
-    ERB.new(template).result(binding)
-  end
-
-  def response_body_for(path)
-    uri = base_fedora_url
-    uri.path = File.join("/fedora-test/objects", path)
-    response = RestClient.get(uri.to_s)
-    response.body
-  end
-
-  def base_fedora_url
-    credentials = ActiveFedora.config.credentials
-    uri = URI(credentials.fetch(:url))
-    uri.user = credentials.fetch(:user)
-    uri.password = credentials.fetch(:password)
-    uri
-  end
-
 end
