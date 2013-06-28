d2beef348cbe643166a8f54dfe1baed51e0fa2e3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-06-28 09:29:00 -0400

    Updating application config to use rspec
    Each Rails application can specify which Framework is used. By default
    the Hydra stack uses RSpec.

--- config/application.rb
+++ config/application.rb
@@ -62,5 +62,9 @@ module HydraCapybaraWalkthrough
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework :rspec, :spec => true, :fixture => false
+    end
   end
 end
